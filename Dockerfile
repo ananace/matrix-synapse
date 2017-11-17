e879ce90fef80de7798fec71dba48016c3cfef97
Alexander "Ace" Olofsson <ace@haxalot.com>
2017-11-17 23:18:32 +0100

    Redoing the docker image to be much lighter
    Also should now support workers much better

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,27 @@
-FROM debian:jessie
+FROM alpine:3.6
 
 MAINTAINER Alexander Olofsson <ace@haxalot.com>
 
-RUN apt-get update -yqq \
-    && apt-get install curl ca-certificates apt-transport-https -yqq --no-install-recommends \
-    && curl https://matrix.org/packages/debian/repo-key.asc | apt-key add - \
-    && echo "deb http://matrix.org/packages/debian/ jessie main" > /etc/apt/sources.list.d/synapse.list \
-    && apt-get update -yqq \
-    && apt-get install matrix-synapse python-matrix-synapse-ldap3 python-psycopg2 -yqq --no-install-recommends \
-    && apt-get autoclean -yqq \
-    && rm -rf /var/lib/apt/
+ARG SYNAPSE_VER=0.25.1
 
-ADD matrix-synapse.sh /usr/local/bin/matrix-synapse
+RUN set -eux \
+    && apk add --no-cache \
+      build-base ca-certificates python2-dev py2-pip su-exec \
+      py2-psycopg2 py2-msgpack py2-psutil py2-openssl py2-yaml py-twisted \
+      py2-netaddr py2-cffi py2-asn1 py2-asn1-modules py2-cryptography \
+      py2-pillow py2-decorator py2-jinja2 py2-requests py2-simplejson py2-tz \
+      py2-crypto py2-dateutil py2-service_identity \
+    && pip install https://github.com/matrix-org/synapse/archive/v$SYNAPSE_VER.tar.gz \
+    && rm -rf /root/.cache \
+    && mkdir -p /synapse/config /synapse/data /synapse/keys \
+    && addgroup -g 666 -S synapse \
+    && adduser -u 666 -S -G synapse -h /synapse/config synapse
+
+ADD log.yaml /synapse
+ADD matrix-synapse.sh /matrix-synapse
+VOLUME /synapse/config /synapse/data /synapse/keys
+
+RUN chown -R synapse:synapse /synapse
 
 EXPOSE 8008 8448
-ENTRYPOINT [ "/usr/local/bin/matrix-synapse" ]
+ENTRYPOINT [ "/matrix-synapse" ]
--- matrix-synapse.sh
+++ matrix-synapse.sh
@@ -1,107 +1,74 @@
-#!/bin/bash -eu
+#!/bin/sh -eu
 
-function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+APP="synapse.app.homeserver"
+ARGS=""
+GEN=""
 
-mkdir -p /kubeconf.d
-mkdir -p /var/lib/matrix-synapse/signing
-
-# This needs to exist, so don't allow empty vars
-echo "server_name: ${MATRIX_SERVERNAME}" > /etc/matrix-synapse/conf.d/server_name.yaml
-echo "${MATRIX_SIGNKEY}" > /var/lib/matrix-synapse/signing/signing.key
-
-[ -n "${MATRIX_DATABASE-}" ] && (
-  case ${MATRIX_DATABASE} in
-    pg|postgres|postgresql)
-      cat <<EOF > /etc/matrix-synapse/conf.d/database.yaml
-database:
-  name: psycopg2
-  args:
-    user: ${MATRIX_DB_USER:-synapse}
-    password: ${MATRIX_DB_PASSWORD:-synapse}
-    database: ${MATRIX_DB_DATABASE:-synapse}
-    host: ${MATRIX_DB_HOST:-localhost}
-    port: ${MATRIX_DB_PORT:-5432}
-    cp_min: 5
-    cp_max: 10
-EOF
+case "${1:-start}" in
+  # HS
+  start|serve|synapse.app.homeserver)
+    shift
+    ARGS="--config-path=/synapse/config"
     ;;
-    sqlite)
-      cat <<EOF > /etc/matrix-synapse/conf.d/database.yaml
-database:
-  name: sqlite3
-  args:
-    database: "${MATRIX_DB_PATH:-/var/lib/matrix-synapse/homeserver.db}"
-EOF
+
+  # Worker
+  synapse.* )
+    APP="$1"
+    shift
+
+    if [ $# -lt 2 ]; then
+      echo "You need to specify at least \`-c /worker-config.yaml\` if trying to run a worker"
+      echo
+      echo "More information can be found on https://github.com/matrix-org/synapse/blob/master/docs/workers.rst"
+      exit 1
+    fi
     ;;
-  esac
 
-)
+  config)
+    shift
+    ARGS="--generate-config --report-stats ${REPORT_STATS:-yes} -H ${SERVER_NAME}"
+    GEN="true"
 
-[ -n "${MATRIX_LDAPURI-}" ] && [ -n "${MATRIX_LDAPBASE}" ] && cat <<EOF > /etc/matrix-synapse/conf.d/ldap.yaml
-password_providers:
-  - module: 'ldap_auth_provider.LdapAuthProvider'
-    config:
-      enabled: true
-      uri: '${MATRIX_LDAPURI}'
-      base: '${MATRIX_LDAPBASE}'
-      attributes:
-        uid: ${MATRIX_LDAPUIDATTR:-uid}
-        mail: ${MATRIX_LDAPMAILATTR:-mail}
-        name: ${MATRIX_LDAPNAMEATTR:-gecos}
-EOF
+    fixup() {
+      rm /synapse/config/*.log.config
+      sed -i /synapse/config/homeserver.yaml \
+      	-e 's!^tls_certificate_path: "/synapse/config!tls_certificate_path: "/synapse/keys!' \
+      	-e 's!^tls_private_key_path: "/synapse/config!tls_private_key_path: "/synapse/keys!' \
+      	-e 's!^tls_dh_params_path: "/synapse/config!tls_dh_params_path: "/synapse/keys!' \
+      	-e 's!^signing_key_path: "/synapse/config!signing_key_path: "/synapse/keys!' \
+      	-e 's!^log_config: .*!log_config: "/synapse/config/log.yaml"!' \
+      	-e 's!^media_store_path: .*!media_store_path: "/synapse/data/media"!' \
+      	-e 's!^uploads_path: .*!uploads_path: "/synapse/data/uploads"!' \
+      	-e 's!^web_client: True!web_client: False!'
 
-[ -n "${MATRIX_TURNURIS-}" ] && [ -n "${MATRIX_TURNSECRET}" ] && cat <<EOF > /etc/matrix-synapse/conf.d/turn.yaml
-turn_uris: [ "$(join_by '", "' "${MATRIX_TURNURIS[@]}")" ]
-turn_shared_secret: ${MATRIX_TURNSECRET}
-turn_user_lifetime: ${MATRIX_TURNLIFETIME:-86400000}
-turn_allow_guests: ${MATRIX_TURNGUESTS:-False}
-EOF
+      echo "Make sure to look throught the generated homeserver yaml, check that everything looks correct before launching your Synapse."
+      echo "If you need to generate keys, you can do so with the \`docker run ... ananace/matrix-synapse ... keys\` command."
+    }
+    trap fixup EXIT
+    ;;
 
-[ -n "${MATRIX_REPORTSTATS-}" ] && echo "report_stats: ${MATRIX_REPORTSTATS}" > /etc/matrix-synapse/conf.d/stats.yaml
-[ -n "${MATRIX_PUBLICURL-}" ] && echo "public_baseurl: ${MATRIX_PUBLICURL}" > /etc/matrix-synapse/conf.d/publicurl.yaml
-[ -n "${MATRIX_REGISTRATIONSECRET-}" ] \
-    && echo "registration_shared_secret: ${MATRIX_REGISTRATIONSECRCET}" > /etc/matrix-synapse/conf.d/registration_shared_secret.yaml \
-    || echo "registration_shared_secret: $(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 16 | head -1)" > /etc/matrix-synapse/conf.d/registration_shared_secret.yaml
+  keys)
+    shift
+    ARGS="--generate-keys --keys-directory /synapse/keys"
+    ;;
 
-[ -n "${MATRIX_INGRESS-}" ] && cat <<EOF > /etc/matrix-synapse/conf.d/listeners.yaml
-listeners:
-- port: 8448
-  bind_address: ''
-  type: http
-  tls: true
-  x_forwarded: true
-  resources:
-  - names:
-    - client
-    - webclient
-    compress: true
-  - names:
-    - federation
-    compress: false
-- port: 8008
-  bind_address: ''
-  type: http
-  tls: false
-  x_forwarded: true
-  resources:
-  - names:
-    - client
-    - webclient
-    compress: true
-  - names:
-    - federation
-    copmress: false
-EOF
+  *)
+    exec "$@"
+    ;;
+esac
 
-echo 'media_store_path: "/var/lib/matrix-synapse/data"' > /etc/matrix-synapse/conf.d/media.yaml
+if [ ! -f /synapse/config/homeserver.yaml ] && [ -z "$GEN" ]; then
+  echo "Missing /synapse/config/homeserver.yaml, you need to generate a configuration before you can run the homeserver."
+  echo
+  echo "Try running with \`docker run ... -e SERVER_NAME=example.com ananace/matrix-synapse:... config\` to let it generate your configuration."
+  exit 1
+fi
+ 
+if [ ! -e /synapse/config/log.yaml ]; then
+  cp /synapse/log.yaml /synapse/config
+fi
 
-cat <<EOF > /etc/matrix-synapse/conf.d/tls.yaml
-tls_certificate_path: "/var/lib/matrix-synapse/tls/tls.crt"
-tls_private_key_path: "/var/lib/matrix-synapse/tls/tls.key"
-tls_dh_params_path:   "/var/lib/matrix-synapse/tls_dh/dhparams.pam"
-signing_key_path:     "/var/lib/matrix-synapse/signing/signing.key"
-EOF
-
-touch /var/log/matrix-synapse/homserver.log
-(tail -F /var/log/matrix-synapse/homeserver.log &)
-python -m synapse.app.homeserver --config-path=/etc/matrix-synapse/homeserver.yaml --config-path=/etc/matrix-synapse/conf.d/ --config-path=/kubeconf.d/ "$@"
+chown -R synapse:synapse /synapse
+echo "> python -m $APP -c /synapse/config/homeserver.yaml $ARGS $*"
+su-exec synapse \
+  python -B -m "$APP" -c /synapse/config/homeserver.yaml $ARGS "$@"
